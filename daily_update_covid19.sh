42550a57f17b94cefd17d7e098456f95bab904a1
Audhi Aprilliant <audhiaprilliant@gmail.com>
2020-06-01 05:02:34 +0700

    Bash command

--- daily_update_covid19.sh
+++ daily_update_covid19.sh
@@ -11,6 +11,7 @@ sleep 5
 
 # Git add commit push
 git add .
+git commit ${dir_covid}/test.out -m "Update monitoring data $current_date"
 git commit ${dir_covid}/Datasets/summary_covid19.txt -m "Update summary data $current_date"
 git commit ${dir_covid}/Datasets/daily_update_covid.csv -m "Update provinces data $current_date"
 git push origin master
