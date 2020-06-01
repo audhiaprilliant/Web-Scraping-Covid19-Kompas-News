9dd1d517582e72cd37ece9a7cf960c6c06cfeec9
Audhi Aprilliant <audhiaprilliant@gmail.com>
2020-06-01 20:30:04 +0700

    Update for developing daily update

--- daily_update_covid19.sh
+++ daily_update_covid19.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Directory
-dir_covid=$(pwd)
+dir_covid=/home/audhi/github/Web-Scraping-Covid19-Kompas-News
 # Run the python program
 cd $dir_covid && /usr/bin/python3 'Web Scraping Covid-19 Kompas News.py' >> test.out
 
 # Sleep 5 seconds
 sleep 5
 current_date=$(tail -n 1 ${dir_covid}/Datasets/summary_covid19.txt | cut -d, -f1)
-# Git add commit push
-git add .
-git commit ${dir_covid}/test.out -m "Update monitoring data $current_date"
-git commit ${dir_covid}/Datasets/summary_covid19.txt -m "Update summary data $current_date"
-git commit ${dir_covid}/Datasets/daily_update_covid.csv -m "Update provinces data $current_date"
-git push origin master
+# Git add commit change
+cd $dir_covid /usr/bin/git add .
+/usr/bin/git commit ${dir_covid}/test.out -m "Update monitoring data $current_date"
+/usr/bin/git commit ${dir_covid}/Datasets/summary_covid19.txt -m "Update summary data $current_date"
+/usr/bin/git commit ${dir_covid}/Datasets/daily_update_covid.csv -m "Update provinces data $current_date"
+# Git commit
+cd $dir_covid /usr/bin/git push origin master
