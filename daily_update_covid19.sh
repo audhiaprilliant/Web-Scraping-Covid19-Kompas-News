8f4188fd202bd63999fc1652bb9f8005bf1161b1
Audhi Aprilliant <audhiaprilliant@gmail.com>
2020-06-04 00:20:01 +0700

    Updated

--- daily_update_covid19.sh
+++ daily_update_covid19.sh
@@ -11,9 +11,9 @@ current_date=$(tail -n 1 ${dir_covid}/Datasets/summary_covid19.txt | cut -d, -f1
 # Git add commit change
 echo $date_run
 cd $dir_covid && /usr/bin/git add --all
-/usr/bin/git commit ${dir_covid}/test.out -m Update monitoring data $current_date"
-/usr/bin/git commit ${dir_covid}/Datasets/summary_covid19.txt -m Update summary data $current_date"
-/usr/bin/git commit ${dir_covid}/Datasets/daily_update_covid.csv -m Update provinces data $current_date"
+/usr/bin/git commit ${dir_covid}/test.out -m "Update monitoring data $current_date"
+/usr/bin/git commit ${dir_covid}/Datasets/summary_covid19.txt -m "Update summary data $current_date"
+/usr/bin/git commit ${dir_covid}/Datasets/daily_update_covid.csv -m "Update provinces data $current_date"
 # Git commit
 cd $dir_covid && /usr/bin/git push origin master
 echo ""
